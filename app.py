4f3a7f1311dc885bb838108275ec6875d8b01901
gokul687 <gokuraj689@gmail.com>
2025-05-18 21:41:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def main():
                 st.markdown(response)
                 st.session_state.messages.append({"role": "assistant", "content": response})
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
